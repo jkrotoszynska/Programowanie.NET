940dfb01828a2e457e86b99b50991cf9c35eba98
jkrotoszynska <justyna.krotoszynska@gmail.com>
2022-04-30 19:01:31 +0200

    lab02

--- lab02-15.03/lab02-15.03/Program.cs
+++ lab02-15.03/lab02-15.03/Program.cs
@@ -5,61 +5,70 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace lab02_15._03
+
+///Napisz klasę zawierającą dwa pola: pierwsze typu double i drugie typu char(np. liczba i znak).
+//Dopisz pięć przeciążonych konstruktorów: pierwszy przyjmujący jeden argument typu double, drugi przyjmujący jeden argument typu char,
+//trzeci przyjmujący dwa argumenty — pierwszy typu double, drugi typu char — i czwarty przyjmujący również dwa argumenty typu typu double.
+//Czwarty konstruktor niech liczy NWD Euklidesa. Piąty konstruktor (przyjmuje tablice int) Sito Erastotenesa – liczby pierwsze
 {
+    public class Konstruktor
+    {
+        double liczba = 1;
+        char znak = 'a';
+
+        // pierwszy przyjmujący jeden argument typu double
+        public Konstruktor(double arg1)
+        {
+            liczba += arg1;
+            Console.WriteLine("Konstruktor1: " + liczba);
+        }
+
+        // drugi przyjmujący jeden argument typu char
+        public Konstruktor(char arg2)
+        {
+            string ciag = znak.ToString() + arg2.ToString();
+            Console.WriteLine("Konstruktor2: " + ciag);
+        }
+
+        // trzeci przyjmujący dwa argumenty — pierwszy typu double, drugi typu char
+        public Konstruktor(double a, char b)
+        {
+            liczba += a;
+            string ciag = znak.ToString() + b.ToString();
+            Console.WriteLine("Konstruktor3: " + liczba + " oraz " + ciag);
+        }
+
+        // czwarty przyjmujący również dwa argumenty typu typu double - niech liczy NWD Euklidesa.
+        public Konstruktor(double num1, double num2)
+        {
+            while (num1 != num2)
+            {
+                if (num1 > num2)
+                    num1 -= num2;
+                else
+                    num2 -= num1;
+            }
+            Console.WriteLine("Konstruktor4: " + num1);
+        }
+
+        // piąty konstruktor (przyjmuje tablice int) Sito Erastotenesa – liczby pierwsze
+        public Konstruktor(int[] arr)
+        {
+
+        }
+
+    }
+
     class Program
     {
-        double x;
-        char y;
-
-        public Program(double x, char y)
+        public static void Main(string[] args)
         {
-            this.x = x;
-            this.y = y;
-        }
-
-        public void MetodaDane(double x)
-        {
-            Console.Write("x = " + x);
-        }
-
-        public void MetodaDane(char y)
-        {
-            Console.Write("y = " + y);
-        }
-
-        public void MetodaDane(double x, char y)
-        {
-            Console.Write("x = " + x);
-            Console.Write("y = " + y);
-        }
-
-        public static void MetodaDane(double x, double x1)
-        {
-            while (x != x1)
-            {
-                if (x > x1)
-                {
-                    x = x - x1;
-                }
-                else
-                {
-                    x1 = x1 - x;
-                }
-            }
-            Console.WriteLine(x);
-        }
-
-       /* public void MetodaDane(int[] Array)
-        {
-
-        }*/
-
-        static void Main(string[] args)
-        {/*
-            Program program1 = new Program(50.5, 'a');
-            Program program2 = new Program(60.6, 'b');*/
-
-            Program program4 = new Program(50.5, 10.1);
+            Konstruktor konstruktor1 = new Konstruktor(2.0);
+            Konstruktor konstruktor2 = new Konstruktor('b');
+            Konstruktor konstruktor3 = new Konstruktor(5.1, 'Z');
+            Konstruktor konstruktor4 = new Konstruktor(36.0, 8.0);
+            Konstruktor konstruktor5 = new Konstruktor(new int[] { 1, 3, 5, 7, 9 });
+            Console.ReadKey();
         }
     }
 }
