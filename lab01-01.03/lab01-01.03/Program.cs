0e118936a347c302ca91dd0580017ebf0778760d
jkrotoszynska <justyna.krotoszynska@gmail.com>
2022-04-30 13:30:18 +0200

    first

---
+++ lab01-01.03/lab01-01.03/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace lab01_01._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadanie 1");
+            // Wyświetlanie liczb od 100 do 1, podzielnych przez 3 i niepodzielnych przez 2 - FOR
+            for (int i = 100; i > 0; i--)
+            {
+                if (i % 3 == 0 && i % 2 != 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+            Console.WriteLine(" ");
+            // Wyświetlanie liczb od 100 do 1, podzielnych przez 3 i niepodzielnych przez 2 - WHILE
+            int j = 100;
+            while (j > 0)
+            {
+                if (j % 3 == 0 && j % 2 != 0)
+                {
+                    Console.Write(j + " ");
+                }
+                j--;
+            }
+
+            Console.WriteLine(" ");
+            // Wyświetlanie liczb od 100 do 1, podzielnych przez 3 i niepodzielnych przez 2 - DO ... WHILE
+            int k = 100;
+            do
+            {
+                if( k % 3 == 0 && k % 2 != 0)
+                {
+                    Console.Write(k + " ");
+                }
+                k--;
+            } while (k > 0);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Zadanie 2");
+
+            int[][] tab = new int[4][];
+            tab[0] = new int[1];
+            tab[1] = new int[2];
+            tab[2] = new int[3];
+            tab[3] = new int[4];
+
+            int licznik = 1;
+            for (int i = 0; i < tab.Length; i++)
+            { 
+                for (int x = 0; x < tab[i].Length; x++)
+                {
+                    
+                }
+            }
+            Console.Write(tab);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Zadanie 3");
+        }
+    }
+}
---
+++ lab02-15.03/lab02-15.03/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab02_15._03
+{
+    class Program
+    {
+        double x;
+        char y;
+
+        public Program(double x, char y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void MetodaDane(double x)
+        {
+            Console.Write("x = " + x);
+        }
+
+        public void MetodaDane(char y)
+        {
+            Console.Write("y = " + y);
+        }
+
+        public void MetodaDane(double x, char y)
+        {
+            Console.Write("x = " + x);
+            Console.Write("y = " + y);
+        }
+
+        public static void MetodaDane(double x, double x1)
+        {
+            while (x != x1)
+            {
+                if (x > x1)
+                {
+                    x = x - x1;
+                }
+                else
+                {
+                    x1 = x1 - x;
+                }
+            }
+            Console.WriteLine(x);
+        }
+
+       /* public void MetodaDane(int[] Array)
+        {
+
+        }*/
+
+        static void Main(string[] args)
+        {/*
+            Program program1 = new Program(50.5, 'a');
+            Program program2 = new Program(60.6, 'b');*/
+
+            Program program4 = new Program(50.5, 10.1);
+        }
+    }
+}
