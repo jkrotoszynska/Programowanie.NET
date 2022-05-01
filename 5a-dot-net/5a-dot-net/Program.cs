7eab3a44acaf23f46beb13e91a467e0b4257196a
jkrotoszynska <justyna.krotoszynska@gmail.com>
2022-05-02 00:36:51 +0200

    2 finish

---
+++ 5a-dot-net/5a-dot-net/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace _5a_dot_net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Napisz program, który będzie realizował tzw. szyfr Cezara (modyfikacja o 1) działający na znakach wprowadzanych bezpośrednio z klawiatury.
+            // Naciśnięcie klawisza odpowiadającego literze a ma powodować pojawianie się litery o podany przez użytkownika klucz.
+            // Możesz ograniczyć się do przekodowywania tylko małych liter z alfabetu łacińskiego. (q lub escape kończy działanie)
+            void Zadanie1()
+            {
+
+            }
+
+            // Napisz program rozwiązujący równania kwadratowe, w którym parametry A, B i C będą wprowadzane przez użytkownika z klawiatury.
+            void Zadanie2()
+            {
+                Console.Write("Podaj A: ");
+                string a = Console.ReadLine();
+                int A = int.Parse(a);
+
+                Console.Write("Podaj B: ");
+                string b = Console.ReadLine();
+                int B = int.Parse(b);
+
+                Console.Write("Podaj C: ");
+                string c = Console.ReadLine();
+                int C = int.Parse(c); 
+
+                Console.WriteLine(A + "x^2 + " + B + "x + " + C);
+                double delta = (B * B) - (4 * A * C);
+                Console.WriteLine("Delta: " + delta);
+
+                if(delta > 0)
+                {
+                    double x1 = ((-1 * B) - Math.Sqrt(delta)) / (2 * A);
+                    double x2 = ((-1 * B) + Math.Sqrt(delta)) / (2 * A);
+                    Console.WriteLine("Funkcja posiada dwa miejsca zerowe: " + x1 +" i " + x2);
+
+                }
+                else if(delta == 0)
+                {
+                    int x = (-1 * B) / (2 * A);
+                    Console.WriteLine("Funkcja posiada jedno miejsce zerowe: " + x);
+                }
+                else
+                {
+                    Console.WriteLine("Brak miejsc zerowych");
+                }
+            }
+
+            // Napisz program, który umożliwi użytkownikowi wprowadzenie wiersza tekstu zawierającego liczby całkowite oddzielone znakiem separatora
+            // (np. przecinkiem), a więc przykładowego ciągu 1,5,24,8,150,2. Program powinien następnie wyświetlić liczy pierwsze (sito Eratostenesa)
+            // (wyszukiwanie liczby najmniejszej – dowolna metodą)
+            void Zadanie3()
+            {
+
+            }
+
+            Zadanie1();
+            Zadanie2();
+            Zadanie3();
+            Console.ReadKey();
+        }
+    }
+}
