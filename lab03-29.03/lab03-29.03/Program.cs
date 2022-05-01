803cb73d6fadb6d0903727406b6aa437dbe6bbf1
jkrotoszynska <justyna.krotoszynska@gmail.com>
2022-05-01 17:23:15 +0200

    lab03 done

--- lab03-29.03/lab03-29.03/Program.cs
+++ lab03-29.03/lab03-29.03/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using System;
+using System.Windows.Forms;
 
 namespace lab03_29._03
 {
@@ -6,7 +8,34 @@ namespace lab03_29._03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Napisz przykładowy program, w którym zostaną wygenerowane dwa różne wyjątki (ArithmeticException, Exception).
+            // Wyświetl na ekranie systemowe komunikaty, (JAK TO ZROBIĆ)?
+            void Zadanie1(){
+                try
+                {
+                    int liczbaA = 50;
+                    int liczbaB = 0;
+                    int dzielenie = liczbaA / liczbaB;
+                }
+
+                catch (DivideByZeroException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (SystemException e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+
+            }
+
+            //Napisz program, który pobierze od użytkownika tablice 10 liczb całkowitych. Przekaz tablicę do konstruktora,
+            //który posortuje tablicę metodą bąbelkową, i zwróci tablicę do metody main().
+            //Wykorzystaj konstrukcję try – catch (np. przekroczenie zakresu tablicy).
+            void Zadanie2()
+            {
+
+            }
         }
     }
 }
