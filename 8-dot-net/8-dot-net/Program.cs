69b339101bef927e000e5db7aaeeca80121b2f66
jkrotoszynska <justyna.krotoszynska@gmail.com>
2022-05-02 22:46:09 +0200

    8 done

--- 8-dot-net/8-dot-net/Program.cs
+++ 8-dot-net/8-dot-net/Program.cs
@@ -10,13 +10,38 @@ namespace _8_dot_net
     {
         static void Main(string[] args)
         {
-            // Napisz program, obliczający przybliżoną wartość liczby PI z wykorzystaniem metody Monte Carlo, a następnie uzyskaną wartość zapisać do dowolnego pliku.
+            // Napisz program, obliczający przybliżoną wartość liczby PI z wykorzystaniem metody Monte Carlo,
+            // a następnie uzyskaną wartość zapisać do dowolnego pliku.
             void zadanie()
             {
+                Console.WriteLine("Podaj liczbę punktów n: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int k = 0;
+                double x, y;
+                Random rand = new Random();
 
+                if(n < 3)
+                {
+                    Console.WriteLine("Błąd!");
+                }
+                else
+                {
+                    for(int i = 0; i < n; i++)
+                    {
+                        x = rand.NextDouble() * 2;
+                        y = rand.NextDouble() * 2;
+                        if(Math.Sqrt((x-1)*(x-1)+(y-1)*(y-1)) <= 1)
+                        {
+                            k++;
+                        }
+                    }
+                    decimal returnVal = 4 * k / (decimal)n;
+                    Console.WriteLine("Liczba PI w przybliżeniu wynosi: " + returnVal.ToString());
+                }
             }
 
             zadanie();
+            Console.ReadKey();
         }
     }
 }
