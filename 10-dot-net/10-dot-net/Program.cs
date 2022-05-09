632145e6b6e8c67eb941abba880785b84836dfca
jkrotoszynska <justyna.krotoszynska@gmail.com>
2022-05-09 14:26:51 +0200

    done

--- 10-dot-net/10-dot-net/Program.cs
+++ 10-dot-net/10-dot-net/Program.cs
@@ -10,16 +10,69 @@ namespace _10_dot_net
     {
         static void Main(string[] args)
         {
-            // Napisz klasę First zawierającą abstrakcyjną metodę f i wirtualną r, z tej klasy wyprowadź klasę potomną Second zawierającą ciała metod f i r na podstawie metod f i r klasy bazowej. 
-            // Rozkład na czynniki pierwsze(Fermata lub klasyczny)
+            // Napisz klasę First zawierającą abstrakcyjną metodę f i wirtualną r,
+            // z tej klasy wyprowadź klasę potomną Second zawierającą ciała metod f i r
+            // na podstawie metod f i r klasy bazowej. Rozkład na czynniki pierwsze(Fermata lub klasyczny)
             // RLE(Run Length Encoding)(dokonać kompresji Unnniiiiwwerrrsyyyttttteeeeet Gddddaansssskkii
-
-            void zadanie()
+            void dopisz(ref string str, char pomocna, int licznik)
             {
-
+                if (licznik > 0)
+                {
+                    str += pomocna;
+                }
+                if (licznik > 1)
+                {
+                    str += licznik.ToString();
+                }
             }
 
-            zadanie();
+            string kompresja(string tekst) {
+                string rezultat = "";
+                int licznik = 0;
+                char pomocna = tekst[0];
+                for(int i = 0; i < tekst.Length; i++)
+                {
+                    if(tekst[i] == pomocna)
+                    {
+                        licznik++;
+                    }
+                    else
+                    {
+                        dopisz(ref rezultat, pomocna, licznik);
+                        pomocna = tekst[i];
+                        licznik = 1;
+                    }
+                }
+                dopisz(ref rezultat, pomocna, licznik);
+                return rezultat;
+            }
+
+            
+            Console.WriteLine(kompresja("Unnnniiiiiwweeerrrekkkkk"));
+            Console.WriteLine(kompresja("Unnniiiiwwerrrsyyyttttteeeeet"));
+            Console.WriteLine(kompresja("Gddddaansssskkii"));
+            Console.ReadKey();
+        }   
+
+    }
+    public abstract class First
+    {
+        public abstract void f();
+        public virtual String r() {
+            return "Zakończono";
+        }
+    }
+
+    public abstract class Second : First
+    {
+        public override void f()
+        {
+            Console.WriteLine("Hello world!!!");
+        }
+
+        public override String r()
+        {
+            return "Zakończono program";
         }
     }
 }
