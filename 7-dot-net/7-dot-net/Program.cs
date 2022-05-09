9dc804329c8c4fa4a9c4f37f65297cf4ac484003
jkrotoszynska <justyna.krotoszynska@gmail.com>
2022-05-09 21:12:34 +0200

    update 3

--- 7-dot-net/7-dot-net/Program.cs
+++ 7-dot-net/7-dot-net/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,11 +18,24 @@ namespace _7_dot_net
                 string katalog  = Console.ReadLine();
                 Console.Write("Podaj nazwę pliku: ");
                 string plik = Console.ReadLine();
+                plik += ".txt";
+
+                //Directory.CreateDirectory(katalog);
+                //FileStream fs = new FileStream(plik, FileMode.Create, FileAccess.ReadWrite);
+                //for(int i = 0; i < 10; i++)
+                //{
+                //    fs.WriteByte((byte)i);
+                //}
+                //fs.Position = 0;
+                //for(int j = 0; j < 10; j++)
+                //{
+                //    Console.Write(fs.ReadByte() + " ");
+                //}
+                //fs.Close();
+
 
                 Console.WriteLine(" ");
             }
-
-            
 
             // Napisz program usuwający plik lub katalog o nazwie przekazanej z wiersza poleceń.
             // Program powinien zapytać użytkownika o potwierdzenie chęci wykonania tej operacji
@@ -40,22 +54,24 @@ namespace _7_dot_net
                     tn = Console.ReadLine();
                     if(tn == "tak")
                     {
-
+                        Directory.Delete(nazwa);
                     }
                     else
                     {
-                        Environment.Exit(0);
+                        Console.WriteLine("Próba usnięcia katalogu zakończona niepowodzeniem");
+                        //Environment.Exit(0);
                     }
                 }else if(odpowiedz == "plik"){
                     Console.WriteLine("Czy chcesz usunąć plik o nazwie {0}? tak/nie", nazwa);
                     tn = Console.ReadLine();
                     if (tn == "tak")
                     {
-
+                        File.Delete(nazwa);
                     }
                     else
                     {
-                        Environment.Exit(0);
+                        Console.WriteLine("Próba usnięcia pliku zakończona niepowodzeniem");
+                        //Environment.Exit(0);
                     }
                 }
                 else
@@ -67,13 +83,27 @@ namespace _7_dot_net
 
             // Napisz program wyświetlający sumaryczną wielkość plików zawartych w katalogu o nazwie przekazanej z wiersza poleceń.
             void Zadanie3() {
-                Console.Write("Podaj nazwe katalogu");
-                string katalog = Console.ReadLine();
-            }
+                Console.Write("Podaj nazwe katalogu: ");
+                string folder = Console.ReadLine();
+                long suma = 0;
 
-            Zadanie1();
-            Zadanie2();
+                DirectoryInfo dir = new DirectoryInfo(folder);
+                FileInfo[] fileArr = dir.GetFiles();
+
+                Console.WriteLine("Folder o nazwie {0} zawiera: ", dir.Name);
+                foreach(FileInfo fi in fileArr)
+                {
+                    Console.WriteLine("Wielkość pliku {0} wynosi {1}", fi.Name, fi.Length);
+                    suma += fi.Length;
+                }
+
+                Console.WriteLine("Ich łączna wielkość to: " + suma);
+             }
+
+            //Zadanie1();
+            //Zadanie2();
             Zadanie3();
+            Console.ReadKey();
         }
     }
 }
