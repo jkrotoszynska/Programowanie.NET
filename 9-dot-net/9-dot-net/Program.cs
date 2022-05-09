85d3af0232e1347a1e0de65d955c9e95a46dc3d9
jkrotoszynska <justyna.krotoszynska@gmail.com>
2022-05-09 22:41:16 +0200

    ...

--- 9-dot-net/9-dot-net/Program.cs
+++ 9-dot-net/9-dot-net/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,19 +9,33 @@ namespace _9_dot_net
 {
     class Program
     {
+        // Napisz program wyszukujący palindromy w 10 ciągach znakowych (jeden pod drugim), znajdujących się w pliku.
+        // Odszukaj je wypisz na standardowe wyjście.  (wykorzystaj StreamReader).
+        // Zmienną do której wczytasz słowo, zadeklaruj z użyciem modyfikatora private.
+        // Pokaż, że dostęp jest możliwy z użyciem metody danej klasy, a nie bezpośrednio z metody main() innej klasy.
         static void Main(string[] args)
         {
-            // Napisz program wyszukujący palindromy w 10 ciągach znakowych (jeden pod drugim), znajdujących się w pliku.
-            // Odszukaj je wypisz na standardowe wyjście.  (wykorzystaj StreamReader).
-            // Zmienną do której wczytasz słowo, zadeklaruj z użyciem modyfikatora private.
-            // Pokaż, że dostęp jest możliwy z użyciem metody danej klasy, a nie  bezpośrednio z metody main() innej klasy.
-
-            void zadanie()
+            void zadanie() 
             {
-
+                Console.WriteLine("Podaj nazwę pliku: ");
+                string fileName = Console.ReadLine();
+                try
+                {
+                    using (StreamReader reader = new StreamReader(fileName))
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
+                catch (Exception exp)
+                {
+                    Console.WriteLine(exp.Message);
+                }
+                Console.ReadKey();
             }
-
-            zadanie();
         }
     }
 }
