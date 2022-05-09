7eeffad0068cd85dd4c3245daa80e5ea6d8ab235
jkrotoszynska <justyna.krotoszynska@gmail.com>
2022-05-09 17:52:57 +0200

    done

--- 11-dot-net/11-dot-net/Program.cs
+++ 11-dot-net/11-dot-net/Program.cs
@@ -6,21 +6,80 @@ using System.Threading.Tasks;
 
 namespace _11_dot_net
 {
-    class Program
+    public interface First
+    {
+        int a
+        {
+            get;
+            set;
+        }
+        int b
+        {
+            get;
+            set;
+        }
+    }
+
+    public class Liczby : First
+    {
+        private int liczbaA, liczbaB;
+        public int a
+        {
+            get
+            {
+                return liczbaA;
+            }
+            set
+            {
+                liczbaA = value;
+            }
+        }
+        public int b
+        {
+            get
+            {
+                return liczbaB;
+            }
+            set
+            {
+                liczbaB = value;
+            }
+        }
+    }
+    class Program 
     {
         static void Main(string[] args)
         {
             // Napisz klasę która dziedziczy z interfejsu (który zawiera opis właściwości dwóch liczb oraz definicję metody NWW).
             // Następnie w nowej klasie (metodzie main()) zaimplementuj algorytm NWW.
             // NWW(a, b) = a⋅b / NWD(a, b)
-            
-            void zadanie()
-            {
 
+            Liczby dane = new Liczby();
+            dane.a = 2;
+            dane.b = 5;
+            Console.WriteLine("Pierwsza liczba to {0}", dane.a);
+            Console.WriteLine("Druga liczba to {0}", dane.b);
+
+
+            int NWW(int a, int b)
+            {
+                if (a < b)
+                {
+                    return NWW(b, a);
+                }
+                else
+                {
+                    int k = a;
+                    while(k%b != 0)
+                    {
+                        k += a;
+                    } 
+                    return k;
+                }
             }
 
-            zadanie();
-
+            Console.WriteLine("NWW to "+ NWW(dane.a, dane.b));
+            Console.ReadKey();
         }
     }
 }
